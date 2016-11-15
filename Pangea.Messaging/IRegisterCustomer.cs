fe9454503809fc7fa0de7d592fb35cd15dea5586
Edward Miller <edward@gopangea.com>
2016-11-15 10:24:35 -0600

    Add message interface

---
+++ Pangea.Messaging/IRegisterCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pangea.Messaging
+{
+    public interface IRegisterCustomer
+    {
+        Guid Id { get; }
+        DateTime RegisteredUtc { get; }
+        int Type { get; }
+        string Name { get; }
+        bool Preferred { get; }
+        decimal DefaultDiscount { get; }
+        string Address { get; }
+    }
+}
