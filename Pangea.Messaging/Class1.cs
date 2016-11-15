6f71ba3b3020dce2e8abd6eec206353a4c86f31b
Edward Miller <edward@gopangea.com>
2016-11-15 10:21:02 -0600

    Initial setup

---
+++ Pangea.Messaging/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pangea.Messaging
+{
+    public class Class1
+    {
+    }
+}
