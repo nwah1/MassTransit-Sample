909538dbec460267072b78488e9cc3cb5f975b12
Edward Miller <edward@gopangea.com>
2016-12-02 14:56:04 -0600

    Update connection details

--- Demo.Model/Consts.cs
+++ Demo.Model/Consts.cs
@@ -2,11 +2,11 @@
 {
     public static class Consts
     {
-        public const string RabbitMqAddress = "rabbitmq://localhost:5672";
+        public const string RabbitMqAddress = "rabbitmq://machost:5672";
         public const string LoadDataQueue = "load.data";
         public const string ListReposQueue = "list.repos";
-        public const string User = "guest";
-        public const string Pass = "guest";
+        public const string User = "worker";
+        public const string Pass = "worker";
         public const string Source = @"http://api.github.com/orgs/gopangea/repos";
     }
 }
